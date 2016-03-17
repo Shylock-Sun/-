4030444a17696f6de55890e8b9eb2a06296a5bde
sy <sy@sydemacbook-pro.local>
2016-03-17 18:24:11 +0800

    基本配置

---
+++ 百思不得姐/百思不得姐/SYTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  SYTabBarController.h
+//  百思不得姐
+//
+//  Created by sy on 16/3/17.
+//  Copyright © 2016年 sy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SYTabBarController : UITabBarController
+
+@end
